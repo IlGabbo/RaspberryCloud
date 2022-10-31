ae60502727941b1a151643ad04a40d01c1fac755
zMyster <96393534+IlGabbo@users.noreply.github.com>
2022-10-31 17:51:10 +0100

    Delete model directory

--- model/php/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dirpath = "../dashboard/uploads/";
-    $file_to_delete = $_GET['file'];
-    $string = $dirpath.$file_to_delete;
-    exec("rm '$string'");
-    header("location: showfiles.php");
-?>
